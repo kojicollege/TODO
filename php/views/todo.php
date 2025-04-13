d51e1d703745f21b436503d248180ff8e51440d0
crazymaki <katami4koji.923@gmail.com>
2025-04-13 15:13:30 +0900

    add:TODOの追加機能

--- php/views/todo.php
+++ php/views/todo.php
@@ -1,2 +1,25 @@
-<?php
-echo "TODOアプリ開発スタート！";
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>TODOアプリ</title>
+</head>
+
+<body>
+  <h1>TODOリスト</h1>
+
+  <form action="/add" method="POST">
+    <input type="text" name="task" placeholder="やることを入力" required>
+    <button type="submit">追加</button>
+  </form>
+
+  <h2>やること一覧</h2>
+  <ul>
+    <?php foreach ($todos as $todo): ?>
+    <li><?= htmlspecialchars($todo, ENT_QUOTES, 'UTF-8') ?></li>
+    <?php endforeach; ?>
+  </ul>
+</body>
+
+</html>
